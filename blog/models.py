d65c000a14b138ad5255982db4d1ab3ae02c4b70
ihawiger <ihawiger@gmail.com>
2021-07-09 00:32:37 +0200

    First commit

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField('date published')
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
